60d7feb49d1a87959951ea577c35c1bcd1854880
Russell Morgan <russell.lambie.morgan@gmail.com>
2019-09-13 13:25:37 -0700

    Added boardArray

--- src/ticTacLossOfWillToLive/Game.java
+++ src/ticTacLossOfWillToLive/Game.java
@@ -1,10 +1,16 @@
 package ticTacLossOfWillToLive;
 
 public class Game {
+	
+	public char[][] boardArray = new char[3][3];
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static void gameAlgorithm(int x, int y) {
+		
 	}
 
 }
