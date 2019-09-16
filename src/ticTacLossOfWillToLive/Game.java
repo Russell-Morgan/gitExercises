991b32ab3c516a92a575f90d60978b87049a51d7
Russell Morgan <russell.lambie.morgan@gmail.com>
2019-09-15 17:29:52 -0700

    Updated runCPU, need to handle blocking.

--- src/ticTacLossOfWillToLive/Game.java
+++ src/ticTacLossOfWillToLive/Game.java
@@ -40,7 +40,7 @@ public class Game {
 			winner = player;
 		}
 		
-		runCPU();
+		runCPU(player);
 		if (checkWin()){
 			if (player == 'x') {
 				winner = 'o';
@@ -50,11 +50,57 @@ public class Game {
 		
 	}
 	
-	public static void runCPU() {
+	public static void runCPU(char player) {
 		//handles CPU
+		char cpuToken;
+		if (player == 'x') {
+			cpuToken = 'o';
+		}else {
+			cpuToken = 'x';
+		}
+		int turnCount = 0;
+		
+		for(int r = 0; r != 3; r++) {
+			for(int c = 0; c != 3; c++) {
+				if (boardArray[r][c] == player) {
+					turnCount++;
+				}
+			}
+		}
 		
 		if (boardArray == null) {
+			boardArray[1][1] = cpuToken;
+		}else if (turnCount >= 2) {
 			
+			//check to see if opponent has to be blocked, then block them.
+			if (true) {
+				
+			}else {
+				//if player doesnt have to be blocked
+				
+			}
+			
+		}else if (boardArray[1][1] == '\u0000') {
+			boardArray[1][1] = cpuToken;
+		}else if (boardArray[1][1] != '\u0000') {
+			if (boardArray[0][0] == '\u0000') {
+				boardArray[0][0] = cpuToken;
+			}else if (boardArray[2][0] == '\u0000') {
+				boardArray[2][0] = cpuToken;
+			}else if (boardArray[2][2] == '\u0000') {
+				boardArray[2][2] = cpuToken;
+			}else if (boardArray[0][2] == '\u0000') {
+				boardArray[0][2] = cpuToken;
+			}
+		}else {
+			for(int g = 0; g == 3; g++) {
+				for(int h = 0; h == 3; h++) {
+					if (boardArray[g][h] == '\u0000') {
+						boardArray[g][h] = cpuToken;
+						return;
+					}
+				}
+			}
 		}
 		
 	}
