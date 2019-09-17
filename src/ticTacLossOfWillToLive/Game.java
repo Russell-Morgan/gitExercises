8cf3f13a2d98821a690a97bf22208ed81485e715
Pierre Frigon <pierref97@gmail.com>
2019-09-16 17:24:46 -0700

    I DID IT

--- src/ticTacLossOfWillToLive/Game.java
+++ src/ticTacLossOfWillToLive/Game.java
@@ -1,5 +1,8 @@
 package ticTacLossOfWillToLive;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 	
 	public static char[][] boardArray = new char[3][3];
@@ -102,8 +105,26 @@ public class Game {
 	}
 	
 	public static boolean checkWin() {
-		//returns boolean "true" if x OR o has won, otherwise returns false
-		return false;
+		
+		List<char[]> list = new ArrayList<char[]>(); 
+		
+		char arraywin1[] = {boardArray[0][0], boardArray[0][1], boardArray[0][2]};
+		char arraywin2[] = {boardArray[1][0], boardArray[1][1], boardArray[1][2]};
+		char arraywin3[] = {boardArray[2][0], boardArray[2][1], boardArray[2][2]};
+		char arraywin4[] = {boardArray[0][0], boardArray[1][0], boardArray[2][0]};
+		char arraywin5[] = {boardArray[0][1], boardArray[1][1], boardArray[2][1]};
+		char arraywin6[] = {boardArray[0][2], boardArray[1][2], boardArray[2][2]};
+		char arraywin7[] = {boardArray[0][0], boardArray[1][1], boardArray[2][2]};
+		char arraywin8[] = {boardArray[0][2], boardArray[1][1], boardArray[2][0]};
+		
+		list.add(arraywin1);
+		list.add(arraywin2);
+		list.add(arraywin3);
+		list.add(arraywin4);
+		list.add(arraywin5);
+		list.add(arraywin6);
+		list.add(arraywin7);
+		list.add(arraywin8);
 	}
 
 }
